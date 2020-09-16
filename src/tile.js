40a0e2fddae88a895c5041adfcb0dee87fa4688c
Julien Dargelos <julien.dargelos@me.com>
2020-09-16 23:46:24 +0200

    wip

--- src/tile.js
+++ src/tile.js
@@ -25,7 +25,7 @@ exports.handler = async (event, context) => {
     let i, j, k, v, f, h
 
     for (i = 0; i < width; i++) {
-      const rate = (i / (width - 1) * range + x) * 4
+      const rate = (i / (width - 1) * range + x - 0.5) * 4
       const values = new Array(height).fill(0)
 
       for (j = 0, v = start; j < 1000; j++) {
@@ -36,7 +36,7 @@ exports.handler = async (event, context) => {
         v = v * rate * (1 - v)
         k = 1 - v
 
-        if (k >= y && k <= y + range) {
+        if (k - 0.5 >= y && k - 0.5 <= y + range) {
           k = Math.round((k - y) / range * (height - 1))
           h = values[k]
           values[k] = h + 1

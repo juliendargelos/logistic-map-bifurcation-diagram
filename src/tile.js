48286611d4849fc3e9f6905068790e4571faf22b
Julien Dargelos <julien.dargelos@me.com>
2020-09-16 22:13:06 +0200

    wip

--- src/tile.js
+++ src/tile.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
   let { x = '0', y = '0', z = '1' } = event.queryStringParameters
   const range = 1 / parseInt(z, 10)
 
-  x = parseInt(x, 10) * range
+  x = parseInt(x, 10) * range / 4
   y = parseInt(y, 10) * range
 
   const image = new PNG({ width, height })

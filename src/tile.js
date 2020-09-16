80535ced14f0e8486f9f4c8a17600b7e8e577e79
Julien Dargelos <julien.dargelos@me.com>
2020-09-16 23:06:39 +0200

    wip

--- src/tile.js
+++ src/tile.js
@@ -10,7 +10,7 @@ const backgroundColor = [0, 0, 0]
 
 exports.handler = async (event, context) => {
   let { x = '0', y = '0', z = '1' } = event.queryStringParameters
-  z = Math.exp(parseInt(z, 10)) - Math.E
+  z = Math.exp(parseInt(z, 10)) - Math.E + 1
   const range = 1 / z
 
   x = parseInt(x, 10) * range
